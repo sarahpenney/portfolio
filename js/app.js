003759540353b199bd8e5bc13746c1f7638394be
Sarah Penney <sarahpenneyuk@gmail.com>
2017-01-09 19:41:53 +0000

    project pages done

--- js/app.js
+++ js/app.js
@@ -16,3 +16,21 @@
     window.attachEvent('onmousewheel', scrollHorizontally);
   }
 })();
+
+$(function() {
+  console.log('werk');
+  $('ul.nav a').bind('click',function(event){
+    var $anchor = $(this);
+                    /*
+                    if you want to use one of the easing effects:
+                    $('html, body').stop().animate({
+                        scrollLeft: $($anchor.attr('href')).offset().left
+                    }, 1500,'easeInOutExpo');
+                     */
+    $('html, body').stop().animate({
+      scrollLeft: $($anchor.attr('href')).offset().left
+    }, 1000);
+    console.log('werk2');
+    event.preventDefault();
+  });
+});
